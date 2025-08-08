ba023ea471edc12313890196656fc77f074c1964
Setoguchi487 <setoguchi.yuya.673@s.kyushu-u.ac.jp>
2025-08-08 11:21:07 +0900

    modified

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import othello from './othello';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<othello />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
---
+++ src/othello.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="">
+      delete
+    </div>
+  );
+}
+
+export default App;
